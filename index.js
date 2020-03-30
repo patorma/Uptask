70de519f0480f2c12b221bf5c3a3c94f8a35f2c6
patorma <pocontreras@ing.ucsc.cl>
2020-03-30 12:57:10 -0300

    Agregacion de controlador y vista por pug *  Se agrego controlador asociado a las respectivas rutas. * Se agrego css proporcionado para el curso que se desarrolla

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const routes = require('./routes')
+const morgan = require('morgan');
+const path = require('path'); // nlee lo que es el filesystem osea forma de acceder a carpetas de archivos
+const log = console.log
     // crear una app de express
 
 const app = express();
+app.use(morgan(':method :url :status :response-time ms'))
+
+// habilitar pug
+// set para agregar valor 
+app.set('view engine', 'pug');
+
+// se debe mostrar donde van las listas
+// Añadir la carpeta de las vistas
+// __dirname muestra la carpeta principal en este caso donde esta index 
+app.set('views', path.join(__dirname, './views'))
 
 app.use('/', routes()); // exporta una funcion
 
 
-app.listen(3000);
+app.listen(3000, log(`Server running on port ${3000}`));
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// importar el controlador
+const { proyectosHome } = require('../controller/proyectosController')
+
 module.exports = function() { // equivalente a export default
 
     // ruta para el home
     // cualquier request corre este codigo
-    router.get('/', (req, res) => {
-        res.send('Index');
-    });
-    router.get('/nosotros', (req, res) => {
-        res.send('Nosotros');
-    });
+    router.get('/', proyectosHome);
+
 
     return router;
 }
