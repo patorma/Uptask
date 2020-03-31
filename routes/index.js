d4e90814ad68ed312f6a8569982ac75f9206a752
patorma <pocontreras@ing.ucsc.cl>
2020-03-30 23:53:18 -0300

    Configuracion inicial de vista de nuevoProyecto * Se creo la vista donde ira el formulario del nuevo proyecto * Se asocio esta vista al controlador que esta interactuando con la ruta

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // importar el controlador
-const { proyectosHome } = require('../controller/proyectosController')
+// destructuring
+const { proyectosHome, formularioProyecto } = require('../controller/proyectosController')
 
 module.exports = function() { // equivalente a export default
 
     // ruta para el home
     // cualquier request corre este codigo
     router.get('/', proyectosHome);
-
+    router.get('/nuevo-proyecto', formularioProyecto);
 
     return router;
 }
